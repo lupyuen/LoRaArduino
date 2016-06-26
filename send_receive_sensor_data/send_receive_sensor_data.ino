b49c546daf503cfbae1aa33dcb5e3c01a361854b
lupyuen <lupyuen@gmail.com>
2016-06-27 03:04:41 +0800

    Testing

--- send_receive_sensor_data/send_receive_sensor_data.ino
+++ send_receive_sensor_data/send_receive_sensor_data.ino
@@ -1,52 +1,58 @@
+//  Send sample sensor data to LoRa gateway.  Tested on Dragino LoRa GPS HAT for Raspberry Pi.
 //  Based on https://github.com/matthijskooijman/arduino-lmic
-/*******************************************************************************
- * Copyright (c) 2015 Matthijs Kooijman
- *
- * Permission is hereby granted, free of charge, to anyone
- * obtaining a copy of this document and accompanying files,
- * to do whatever they want with them without any restriction,
- * including, but not limited to, copying, modification and redistribution.
- * NO WARRANTY OF ANY KIND IS PROVIDED.
- *
- * This example transmits data on hardcoded channel and receives data
- * when not transmitting. Running this sketch on two nodes should allow
- * them to communicate.
- *******************************************************************************/
 
+//  TP-IoT Mode 1 is max range. TP-IoT Gateway runs on: CR = 4/5, SF = 12, BW = 125 KHz.
 extern int transmission_mode;
-////  TP-IoT: Mode 1 is max range. TP-IoT Gateway runs on:
-////    case 1:     setCR(CR_5);        // CR = 4/5
-////                setSF(SF_12);       // SF = 12
-////                setBW(BW_125);      // BW = 125 KHz
 int transmission_mode = 1;
+const int device_address = 3;  //  My own address as assigned by TP-IoT.
+const int gateway_address = 1;  //  TP-IoT Gateway address.
+const int TX_INTERVAL = 5000;  //  How often to send a packet, in milliseconds.
+int packet_num = 1;  //  Number of packets sent to gateway so far.
 
 #include <lmic.h>
 #include <hal/hal.h>
 #include <SPI.h>
+osjob_t txjob, timeoutjob;  //  Transmission and timeout tasks.
 
-#if !defined(DISABLE_INVERT_IQ_ON_RX)
-#error This example requires DISABLE_INVERT_IQ_ON_RX to be set. Update \
-       config.h in the lmic library to set it.
-#endif
+static void tx_func (osjob_t* job) {
+  //  Send a message containing sample sensor data to the gateway, delimited by "|".
+  //  The sequence must match lora_gateway.py: temperature, humidity, message.
+  tx("28.1|72|OK", txdone_func);
+  
+  // Send the next set of sensor data every TX_INTERVAL (plus a bit of random to prevent systematic collisions).
+  os_setTimedCallback(job, os_getTime() + ms2osticks(TX_INTERVAL + random(500)), tx_func);
+}
 
-// How often to send a packet. Note that this sketch bypasses the normal
-// LMIC duty cycle limiting, so when you change anything in this sketch
-// (payload length, frequency, spreading factor), be sure to check if
-// this interval should not also be increased.
-// See this spreadsheet for an easy airtime and duty cycle calculator:
-// https://docs.google.com/spreadsheets/d/1voGAtQAjC1qBmaVuP1ApNKs1ekgUjavHuVQIXyYSvNc 
-#define TX_INTERVAL 2000
+// Program starts here.
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Starting");
+  pinMode(LED_BUILTIN, OUTPUT);  //  Allow use of LED.
 
-/*  Original settings:
-// Pin mapping
-const lmic_pinmap lmic_pins = {
-    .nss = 6,
-    .rxtx = LMIC_UNUSED_PIN,
-    .rst = 5,
-    .dio = {2, 3, 4},
-};
-*/
-//  TP-IoT Pin mapping
+  // Initialize runtime env.
+  os_init();
+
+  // Set up these settings once, and use them for both sending and receiving.
+  LMIC.freq = 865200000; // TP-IoT: 865.20 MHz, CH_10_868, central freq = 865.20MHz
+  LMIC.datarate = DR_SF12;  //  TP-IoT Mode 1: Spread Factor 12.
+
+  // Maximum transmission power.
+  LMIC.txpow = 27;
+
+  // This sets the default coding and bandwidth, to be updated in radio.c when we transmit.
+  LMIC.rps = updr2rps(LMIC.datarate);
+  Serial.println("Started"); Serial.flush();
+
+  // First task to be run is to send sensor data.
+  os_setCallback(&txjob, tx_func);
+}
+
+void loop() {
+  // Loop repeatedly to execute scheduled jobs and events.
+  os_runloop_once();
+}
+
+//  TP-IoT Pin mapping for Dragino LoRa GPS HAT.
 const lmic_pinmap lmic_pins = {
     .nss = 10,// Connected to pin D10
     .rxtx = LMIC_UNUSED_PIN,// For placeholder only, Do not connected on RFM92/RFM95
@@ -61,37 +67,42 @@ void os_getArtEui (u1_t* buf) { }
 void os_getDevEui (u1_t* buf) { }
 void os_getDevKey (u1_t* buf) { }
 
-void onEvent (ev_t ev) {
-}
+void onEvent (ev_t ev) { }
 
-osjob_t txjob;
-osjob_t timeoutjob;
-static void tx_func (osjob_t* job);
-
-// Transmit the given string and call the given function afterwards
+// Transmit the given string to the LoRa gateway and call the given function afterwards.
 void tx(const char *str, osjobcb_t func) {
+  //  Header contains destination address, source address, packet number, packet length.
+  uint8_t header[4] = { gateway_address, device_address, packet_num++, strlen(str) };
   os_radio(RADIO_RST); // Stop RX first
   delay(1); // Wait a bit, without this os_radio below asserts, apparently because the state hasn't changed yet
   LMIC.dataLen = 0;
+  //  Copy the header.
+  for (int i = 0; i < sizeof(header); i++)
+    LMIC.frame[LMIC.dataLen++] = header[i];    
+  //  Copy the data.
   while (*str)
     LMIC.frame[LMIC.dataLen++] = *str++;
   LMIC.osjob.func = func;
+  //  Send the data.
+  Serial.print("Sending to gateway ");
+  Serial.print(gateway_address);
+  Serial.print(": ");
+  Serial.println(str);
   os_radio(RADIO_TX);
-  Serial.println("TX");
 }
 
-// Enable rx mode and call func when a packet is received
+// Enable rx mode and call func when a packet is received.
 void rx(osjobcb_t func) {
   LMIC.osjob.func = func;
   LMIC.rxtime = os_getTime(); // RX _now_
-  // Enable "continuous" RX (e.g. without a timeout, still stops after
-  // receiving a packet)
+  // Enable "continuous" RX (e.g. without a timeout, still stops after receiving a packet)
   os_radio(RADIO_RXON);
-  Serial.println("RX");
+  Serial.println("Receiving...");
 }
 
 static void rxtimeout_func(osjob_t *job) {
-  digitalWrite(LED_BUILTIN, LOW); // off
+  //  Switch off the LED on timeout.
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 static void rx_func (osjob_t* job) {
@@ -100,11 +111,10 @@ static void rx_func (osjob_t* job) {
   delay(10);
   digitalWrite(LED_BUILTIN, HIGH); // on
 
+  //  TODO: Test whether this is necessary.
   // Timeout RX (i.e. update led status) after 3 periods without RX
   os_setTimedCallback(&timeoutjob, os_getTime() + ms2osticks(3*TX_INTERVAL), rxtimeout_func);
-
-  // Reschedule TX so that it should not collide with the other side's
-  // next TX
+  // Reschedule TX so that it should not collide with the other side's next TX
   os_setTimedCallback(&txjob, os_getTime() + ms2osticks(TX_INTERVAL/2), tx_func);
 
   Serial.print("Got ");
@@ -121,60 +131,7 @@ static void txdone_func (osjob_t* job) {
   rx(rx_func);
 }
 
-// log text to USART and toggle LED
-static void tx_func (osjob_t* job) {
-  //  Send a message containing sample sensor data to the gateway.
-  tx("\x01\x02\x01\x082|1|2|3", txdone_func);
-  // reschedule job every TX_INTERVAL (plus a bit of random to prevent
-  // systematic collisions), unless packets are received, then rx_func
-  // will reschedule at half this time.
-  os_setTimedCallback(job, os_getTime() + ms2osticks(TX_INTERVAL + random(500)), tx_func);
-}
+#if !defined(DISABLE_INVERT_IQ_ON_RX)
+#error This program requires DISABLE_INVERT_IQ_ON_RX to be set. Update config.h in the lmic library to set it.
+#endif
 
-// application entry point
-void setup() {
-  Serial.begin(9600);
-  Serial.println("Starting");
-  #ifdef VCC_ENABLE
-  // For Pinoccio Scout boards
-  pinMode(VCC_ENABLE, OUTPUT);
-  digitalWrite(VCC_ENABLE, HIGH);
-  delay(1000);
-  #endif
-
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  // initialize runtime env
-  os_init();
-
-  // Set up these settings once, and use them for both TX and RX
-
-  // Use a frequency in the g3 which allows 10% duty cycling.
-  ////LMIC.freq = 869525000;
-  ////  TP-IoT: uint32_t LORA_CH_10_868 = CH_10_868; //  0xD84CCC; // channel 10, central freq = 865.20MHz  ////  Lup Yuen
-  LMIC.freq = 865200000; //// TP-IoT: 865.20 MHz
-  
-  // Maximum TX power
-  LMIC.txpow = 27;
-  // Use a medium spread factor. This can be increased up to SF12 for
-  // better range, but then the interval should be (significantly)
-  // lowered to comply with duty cycle limits as well.
-  ////LMIC.datarate = DR_SF9;
-  LMIC.datarate =
-    (transmission_mode == 1) ? DR_SF12 :  ////  TP-IoT Mode 1.
-    (transmission_mode == 5) ? DR_SF10 :  ////  TP-IoT Mode 5.
-  
-  // This sets CR 4/5, BW125 (except for DR_SF7B, which uses BW250)
-  LMIC.rps = updr2rps(LMIC.datarate);
-
-  Serial.println("Started");
-  Serial.flush();
-
-  // setup initial job
-  os_setCallback(&txjob, tx_func);
-}
-
-void loop() {
-  // execute scheduled jobs and events
-  os_runloop_once();
-}
